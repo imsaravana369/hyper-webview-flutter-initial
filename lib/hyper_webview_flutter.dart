db549509f499f4b2106fdf8e1ba71b6da5d3a51d
Saravanan M <saravanan.m@Saravanan-M-C02G6AAWMD6M.local>
2023-12-26 15:25:05 +0530

    Added Copyrights

--- lib/hyper_webview_flutter.dart
+++ lib/hyper_webview_flutter.dart
@@ -1,4 +1,9 @@
-
+/*
+ * Copyright (c) Juspay Technologies.
+ *
+ * This source code is licensed under the AGPL 3.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'dart:developer';
